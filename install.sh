a20a8f253682a0a66d58ef74cae78c8867bf49df
Timothy Ngo <timothyngo08@gmail.com>
2018-01-12 16:56:30 -0800

    upated install

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ cd ~/.vim
 mkdir temp_dirs && mkdir temp_dirs/undodir
 
 git submodule update --init --recursive
+./pack/plugins/start/YouCompleteMe/install.py --clang-completer --system-libclang
 
 echo "Installed"
