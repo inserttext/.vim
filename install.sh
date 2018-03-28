e615d254f6cb97094086c8a3e1da38ac2ab5b935
Timothy Ngo <timothyngo08@gmail.com>
2018-03-28 11:02:29 -0700

    remove old install

--- install.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-cd ~/.vim
-mkdir temp_dirs && mkdir temp_dirs/undodir
-
-git submodule update --init --recursive
-./pack/plugins/start/YouCompleteMe/install.py --clang-completer --system-libclang
-
-echo "Installed"
